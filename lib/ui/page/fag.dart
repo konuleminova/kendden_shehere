8454ba47da85aeb3496ba061678c1d499b1625a1
konul <konul.eminova39@gmail.com>
2019-05-15 19:10:25 +0400

    added fag

--- lib/ui/page/fag.dart
+++ lib/ui/page/fag.dart
@@ -2,42 +2,106 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/services.dart' show rootBundle;
 
-class FagPage extends StatelessWidget {
+class FagPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return new FAGState();
+  }
+}
+
+class FAGState extends State<FagPage> {
+  double width;
+  List<bool> expanded = new List();
+
+
+  @override
+  void initState() {
+    super.initState();
+    for (int i = 0; i < 12; i++) {
+      expanded.add(false);
+    }
+
+  }
+
   @override
   Widget build(BuildContext context) {
+    width = MediaQuery.of(context).size.width;
+
     // TODO: implement build
     return new Scaffold(
         appBar: new AppBar(
           title: Text("FAG"),
           backgroundColor: Colors.lightGreen,
         ),
-        body: SingleChildScrollView(
-          child: new Container(
-            margin: EdgeInsets.all(16),
-            child: new FutureBuilder(
-                future: getFileData('images/fag.txt'),
-                builder: (BuildContext context, AsyncSnapshot snapshot) {
-                  if (snapshot.hasData) {
-                    if (snapshot.data != null) {
-                      return new Text(
-                        snapshot.data,
-                        style: TextStyle(
-                            color: Colors.green,
-                            fontSize: 22,
-                            fontWeight: FontWeight.bold),
-                      );
-                    }
-                  } else {
-                    return Center(
-                      child: new CircularProgressIndicator(),
-                    );
+        body:new Container(
+          margin: EdgeInsets.all(16),
+          child: new FutureBuilder(
+              future: getFileData('images/fag.txt'),
+              builder: (BuildContext context, AsyncSnapshot snapshot) {
+                if (snapshot.hasData) {
+                  if (snapshot.data != null) {
+                    String text = snapshot.data;
+                   // List<String>  splits =text.split("\n");
+                    return _buildFags(text,expanded);
                   }
-                }),
-          ),
-        ));
+                } else {
+                  return Center(
+                    child: new CircularProgressIndicator(),
+                  );
+                }
+              }),
+        ),);
   }
 
   Future<String> getFileData(String path) async {
     return await rootBundle.loadString(path);
   }
+
+  _buildFagQuestionItem(String text, String subtitle,bool expande,int index) => new Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          GestureDetector(
+            child:Container(child: new Text(
+              text,
+              style: TextStyle(
+                  color: Colors.green,
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold),
+            ),margin: EdgeInsets.only(bottom: 5,top: 5),),
+            onTap: () {
+              setState(() {
+               expande = !expande;
+               expanded[index]=expande;
+              });
+            },
+          ),
+          expande
+              ? (new Container(
+                  margin: EdgeInsets.only(top: 8,bottom: 8),
+                  padding: EdgeInsets.all(20),
+                  width: width,
+                  child: new Text(
+                    subtitle,
+                    style:
+                        TextStyle(fontSize: 18, fontWeight: FontWeight.normal),
+                  ),
+                  decoration: BoxDecoration(
+                      borderRadius: new BorderRadius.circular(5),
+                      border: new Border.all(color: Colors.grey[400])),
+                ))
+              : new SizedBox(
+                  height: 0,
+                  width: 0,
+                )
+        ],
+      );
+
+  _buildFags(String text,List<bool> expanded) => ListView.builder(
+        itemBuilder: (BuildContext context, int index) {
+          return _buildFagQuestionItem(text.split("\n")[index], "Subtitle",expanded[index],index);
+        },
+        itemCount: 12,
+      );
 }
