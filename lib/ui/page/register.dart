9a59aa054ba3ae30f466b79535b6935fef309371
konul <konul.eminova39@gmail.com>
2019-07-04 15:04:31 +0400

    register

--- lib/ui/page/register.dart
+++ lib/ui/page/register.dart
@@ -8,9 +8,56 @@ import 'package:kendden_shehere/redux/register/register_viewmodel.dart';
 import 'package:kendden_shehere/service/networks.dart';
 import 'package:redux/redux.dart';
 
-class RegisterPage extends StatelessWidget {
+class RegisterPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return RegisterPageState();
+  }
+}
+
+class RegisterPageState extends State<RegisterPage> {
   bool status;
   RegisterViewModel viewModel;
+  bool _validateUsername = false;
+  bool _validateName = false;
+  bool _validateSurname = false;
+  bool _validateMobile = false;
+  bool _validatePassword = false;
+  bool _validatePass2 = false;
+  TextEditingController _controllerUsername, _controllerPass, _controllerPass2;
+  TextEditingController _controllerName, _controllerSurname;
+  TextEditingController _controllerMobile;
+  FocusNode userFocus, passFocus;
+  double opacity;
+
+
+  @override
+  void initState() {
+    super.initState();
+    opacity = 0.5;
+    _controllerUsername = TextEditingController();
+    _controllerPass = TextEditingController();
+    _controllerName = TextEditingController();
+    _controllerSurname = TextEditingController();
+    _controllerMobile = TextEditingController();
+    _controllerPass2 = TextEditingController();
+    userFocus = new FocusNode();
+    passFocus = new FocusNode();
+  }
+
+  @override
+  void dispose() {
+    _controllerUsername.dispose();
+    _controllerPass.dispose();
+    _controllerPass2.dispose();
+    _controllerName.dispose();
+    _controllerSurname.dispose();
+    _controllerMobile.dispose();
+
+    super.dispose();
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -94,6 +141,24 @@ class RegisterPage extends StatelessWidget {
                   Container(
                       padding: EdgeInsets.symmetric(horizontal: 20.0),
                       child: TextField(
+                        controller: _controllerUsername,
+                        onChanged: (value) {
+                          passFocus.unfocus();
+                          setState(() {
+                            _controllerUsername.text.isEmpty
+                                ? _validateUsername = false
+                                : _validateUsername = true;
+                            if (_validateUsername &&
+                                _validatePassword &&
+                                _validateName &&
+                                _validateSurname &&
+                                _validateMobile & _validatePass2) {
+                              opacity = 1;
+                            } else {
+                              opacity = 0.5;
+                            }
+                          });
+                        },
                         style: TextStyle(color: Colors.blue),
                         decoration: InputDecoration(
                             hintText: "Username",
@@ -114,6 +179,24 @@ class RegisterPage extends StatelessWidget {
                   Container(
                       padding: EdgeInsets.symmetric(horizontal: 20.0),
                       child: TextField(
+                        controller: _controllerName,
+                        onChanged: (value) {
+                          passFocus.unfocus();
+                          setState(() {
+                            _controllerUsername.text.isEmpty
+                                ? _validateName = false
+                                : _validateName = true;
+                            if (_validateUsername &&
+                                _validatePassword &&
+                                _validateName &&
+                                _validateSurname &&
+                                _validateMobile & _validatePass2) {
+                              opacity = 1;
+                            } else {
+                              opacity = 0.5;
+                            }
+                          });
+                        },
                         style: TextStyle(color: Colors.blue),
                         decoration: InputDecoration(
                             hintText: "Name",
@@ -134,7 +217,25 @@ class RegisterPage extends StatelessWidget {
                   Container(
                       padding: EdgeInsets.symmetric(horizontal: 20.0),
                       child: TextField(
+                        controller: _controllerSurname,
                         style: TextStyle(color: Colors.blue),
+                        onChanged: (value) {
+                          passFocus.unfocus();
+                          setState(() {
+                            _controllerUsername.text.isEmpty
+                                ? _validateSurname = false
+                                : _validateSurname = true;
+                            if (_validateUsername &&
+                                _validatePassword &&
+                                _validateName &&
+                                _validateSurname &&
+                                _validateMobile & _validatePass2) {
+                              opacity = 1;
+                            } else {
+                              opacity = 0.5;
+                            }
+                          });
+                        },
                         decoration: InputDecoration(
                             hintText: "Surname",
                             hintStyle: TextStyle(color: Colors.blue.shade200),
@@ -154,7 +255,25 @@ class RegisterPage extends StatelessWidget {
                   Container(
                       padding: EdgeInsets.symmetric(horizontal: 20.0),
                       child: TextField(
+                        controller: _controllerMobile,
                         style: TextStyle(color: Colors.blue),
+                        onChanged: (value) {
+                          passFocus.unfocus();
+                          setState(() {
+                            _controllerUsername.text.isEmpty
+                                ? _validateMobile = false
+                                : _validateMobile = true;
+                            if (_validateUsername &&
+                                _validatePassword &&
+                                _validateName &&
+                                _validateSurname &&
+                                _validateMobile & _validatePass2) {
+                              opacity = 1;
+                            } else {
+                              opacity = 0.5;
+                            }
+                          });
+                        },
                         decoration: InputDecoration(
                             hintText: "Phone Number",
                             hintStyle: TextStyle(color: Colors.blue.shade200),
@@ -174,7 +293,25 @@ class RegisterPage extends StatelessWidget {
                   Container(
                       padding: EdgeInsets.symmetric(horizontal: 20.0),
                       child: TextField(
+                        controller: _controllerPass,
                         style: TextStyle(color: Colors.blue),
+                        onChanged: (value) {
+                          passFocus.unfocus();
+                          setState(() {
+                            _controllerUsername.text.isEmpty
+                                ? _validatePassword = false
+                                : _validatePassword = true;
+                            if (_validateUsername &&
+                                _validatePassword &&
+                                _validateName &&
+                                _validateSurname &&
+                                _validateMobile & _validatePass2) {
+                              opacity = 1;
+                            } else {
+                              opacity = 0.5;
+                            }
+                          });
+                        },
                         decoration: InputDecoration(
                             hintText: "Password",
                             hintStyle: TextStyle(color: Colors.blue.shade200),
@@ -194,7 +331,25 @@ class RegisterPage extends StatelessWidget {
                   Container(
                       padding: EdgeInsets.symmetric(horizontal: 20.0),
                       child: TextField(
+                        controller: _controllerPass2,
                         style: TextStyle(color: Colors.blue),
+                        onChanged: (value) {
+                          passFocus.unfocus();
+                          setState(() {
+                            _controllerUsername.text.isEmpty
+                                ? _validatePass2 = false
+                                : _validatePass2 = true;
+                            if (_validateUsername &&
+                                _validatePassword &&
+                                _validateName &&
+                                _validateSurname &&
+                                _validateMobile & _validatePass2) {
+                              opacity = 1;
+                            } else {
+                              opacity = 0.5;
+                            }
+                          });
+                        },
                         decoration: InputDecoration(
                             hintText: "Confirm password",
                             hintStyle: TextStyle(color: Colors.blue.shade200),
@@ -218,29 +373,45 @@ class RegisterPage extends StatelessWidget {
               ),
             ),
           ),
-          Container(
-            height: 500,
-            child: RaisedButton(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              color: Colors.green[700],
-              onPressed: () {
-                UserModel userModel = new UserModel();
-                userModel.name = "Ilham";
-                userModel.surname = "Kazimov";
-                userModel.username = "empirer";
-                userModel.password = "12345678";
-                viewModel.buildRegister("0", userModel);
-                // Networks.register("0", userModel);
-              },
-              elevation: 11,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(40.0))),
-              child: Text(
-                "Sign Up",
-                style: TextStyle(color: Colors.white70),
+          Opacity(
+            opacity: opacity,
+            child: Container(
+              height: 500,
+              child: RaisedButton(
+                padding: EdgeInsets.symmetric(vertical: 16.0),
+                color: Colors.green[700],
+                onPressed: () {
+                  passFocus.unfocus();
+                  userFocus.unfocus();
+                  if (_validateUsername &&
+                      _validatePassword &&
+                      _validateMobile &&
+                      _validateSurname &&
+                      _validateName &&
+                      _validatePass2) {
+                    UserModel userModel = new UserModel();
+                    userModel.name = "Ilham";
+                    userModel.surname = "Kazimov";
+                    userModel.username = "empirer";
+                    userModel.password = "12345678";
+                    viewModel.buildRegister("0", userModel);
+                    setState(() {
+                      status = true;
+                    });
+                  }
+
+                  // Networks.register("0", userModel);
+                },
+                elevation: 11,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(40.0))),
+                child: Text(
+                  "Sign Up",
+                  style: TextStyle(color: Colors.white70),
+                ),
               ),
+              alignment: AlignmentDirectional.bottomCenter,
             ),
-            alignment: AlignmentDirectional.bottomCenter,
           )
         ],
       ),
