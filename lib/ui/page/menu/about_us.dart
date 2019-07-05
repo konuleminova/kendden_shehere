7dfada4ca4016eb348086521aec1fd2afd4f3c29
konul <konul.eminova39@gmail.com>
2019-07-05 17:28:04 +0400

    translate

--- lib/ui/page/menu/about_us.dart
+++ lib/ui/page/menu/about_us.dart
@@ -3,8 +3,18 @@ import 'package:flutter/services.dart';
 import 'package:flutter/services.dart' show rootBundle;
 
 class AboutUsPage extends StatelessWidget {
+  String title;
+
   @override
   Widget build(BuildContext context) {
+    String langCode = Localizations.localeOf(context).languageCode;
+    if (langCode == "tr") {
+      title = "assets/desc_az.txt";
+    } else if (langCode == "en") {
+      title = "assets/desc.txt";
+    } else if (langCode == "ru") {
+      title = "assets/desc_ru.txt";
+    }
     // TODO: implement build
     return new Scaffold(
         appBar: new AppBar(
@@ -15,11 +25,14 @@ class AboutUsPage extends StatelessWidget {
           child: new Container(
             margin: EdgeInsets.all(16),
             child: new FutureBuilder(
-                future: getFileData('assets/desc.txt'),
+                future: getFileData(title),
                 builder: (BuildContext context, AsyncSnapshot snapshot) {
                   if (snapshot.hasData) {
                     if (snapshot.data != null) {
-                      return new Text(snapshot.data,style: new TextStyle(fontSize: 18),);
+                      return new Text(
+                        snapshot.data,
+                        style: new TextStyle(fontSize: 18),
+                      );
                     }
                   } else {
                     return Center(
