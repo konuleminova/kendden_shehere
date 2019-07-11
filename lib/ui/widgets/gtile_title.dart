c1156560467a0c390202d4d41b8b375e457dacb8
konul <konul.eminova39@gmail.com>
2019-07-11 12:49:30 +0400

    done grocery details

--- lib/ui/widgets/gtile_title.dart
+++ lib/ui/widgets/gtile_title.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:html2md/html2md.dart' as html2md;
+import 'package:flutter_markdown/flutter_markdown.dart';
 
 class GrocerySubtitle extends StatelessWidget {
   const GrocerySubtitle({
@@ -10,9 +12,10 @@ class GrocerySubtitle extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(text, style: TextStyle(
-      color: Colors.grey
-    ));
+    String markdown = html2md.convert(text);
+    return new MarkdownBody(
+      data: markdown,
+    );
   }
 }
 
@@ -26,9 +29,7 @@ class GroceryTitle extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(text, style: TextStyle(
-      fontWeight: FontWeight.w700,
-      fontSize: 15.0
-    ));
+    return Text(text,
+        style: TextStyle(fontWeight: FontWeight.w700, fontSize: 15.0));
   }
 }
