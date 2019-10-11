4ba2a39ccf960454937f892a0557fe45cb4139e5
konuleminova <konul.eminova39@gmail.com>
2019-10-11 12:58:34 +0400

    remove wish item

--- lib/redux/wishlist/wishlist_reduces.dart
+++ lib/redux/wishlist/wishlist_reduces.dart
@@ -1,18 +1,17 @@
-
 import 'package:kendden_shehere/redux/productlist/new_product_model.dart';
 import 'package:kendden_shehere/redux/wishlist/wishlist_action.dart';
 import 'package:redux/redux.dart';
 
 Reducer<List<NewProduct>> wishListReducer = combineReducers<List<NewProduct>>([
+  new TypedReducer<List<NewProduct>, FetchWishListAction>(fetchWishListAction),
   new TypedReducer<List<NewProduct>, RemoveWishItemAction>(
       removeWishItemReducer),
   new TypedReducer<List<NewProduct>, AddWishItemAction>(addWishItemReducer),
-  new TypedReducer<List<NewProduct>, FetchWishListAction>(fetchWishListAction),
 ]);
 
 List<NewProduct> removeWishItemReducer(
-    List<NewProduct> shopItems, RemoveWishItemAction action) {
-  return List.from(shopItems)..remove(action.removeWishItem);
+    List<NewProduct> state, RemoveWishItemAction action) {
+  return List.from(state)..add(action.removeWishItem);
 }
 
 List<NewProduct> addWishItemReducer(
