3a263782cb93d10bf708d6dba127fa4b2b039978
konuleminova <konul.eminova39@gmail.com>
2019-10-15 17:39:51 +0400

    home done

--- lib/ui/page/grocery/grocery_big_image.dart
+++ lib/ui/page/grocery/grocery_big_image.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:photo_view/photo_view.dart';
+
 class GroceryBigImage extends StatelessWidget {
   String code;
+
   GroceryBigImage({this.code});
 
   @override
@@ -10,8 +12,24 @@ class GroceryBigImage extends StatelessWidget {
     // TODO: implement build
     return new Scaffold(
         backgroundColor: Colors.black87,
-        body: Center(
-            child: new Container(
-                child: PhotoView(imageProvider: NetworkImage(img)))));
+        body: Stack(
+          children: <Widget>[
+            Center(
+                child: new Container(
+                    child: PhotoView(imageProvider: NetworkImage(img)))),
+            Positioned(
+              top: 30.0,
+              left: 16.0,
+              child: CircleAvatar(child: IconButton(
+                color: Colors.white,
+                iconSize: 30,
+                icon: Icon(Icons.clear),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),radius: 24,backgroundColor: Colors.grey,)
+            )
+          ],
+        ));
   }
 }
