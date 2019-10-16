68085bd0f94e7b9130c40cbf131ab84cc0350b4b
konuleminova <konul.eminova39@gmail.com>
2019-10-16 11:23:56 +0400

    banner images

--- lib/ui/page/home.dart
+++ lib/ui/page/home.dart
@@ -1,38 +1,23 @@
-import 'dart:async';
-
-import 'package:async/async.dart';
+import 'package:carousel_pro/carousel_pro.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:kendden_shehere/localization/app_translations.dart';
-import 'package:kendden_shehere/localization/application.dart';
 import 'package:kendden_shehere/redux/app/app_state_model.dart';
 import 'package:kendden_shehere/redux/home/home_viewmodel.dart';
-import 'package:kendden_shehere/redux/home/thunk_home.dart';
-import 'package:kendden_shehere/redux/productlist/new_product_model.dart';
-import 'package:kendden_shehere/redux/productlist/productlist_viewmodel.dart';
-import 'package:kendden_shehere/redux/productlist/products_in_category_model.dart';
-import 'package:kendden_shehere/redux/shoplist/shop_viewmodel.dart';
 import 'package:kendden_shehere/service/networks.dart';
-import 'package:kendden_shehere/ui/animation/slide_left.dart';
 import 'package:kendden_shehere/ui/page/grocery/grocery_shop_list.dart';
 import 'package:kendden_shehere/ui/widgets/list_item/new_list_item/new_glistitem1.dart';
-import 'package:kendden_shehere/util/sharedpref_util.dart';
 import 'package:kendden_shehere/ui/widgets/drawer.dart';
 import 'package:kendden_shehere/ui/widgets/search.dart';
-import 'package:carousel_slider/carousel_slider.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
 import 'package:redux/redux.dart';
 
 class HomePage extends StatelessWidget {
   double height = 0;
   double width = 0;
   List<String> photos = new List();
-  var _current = 0;
   String title;
-  ProductsInCategory productsInCategory = new ProductsInCategory();
 
   @override
   Widget build(BuildContext context) {
@@ -159,12 +144,20 @@ class HomePage extends StatelessWidget {
                                 images.add(new Container(
                                   width: width,
                                   child: new Image(
-                                    image: NetworkImage(photos[i]),
+                                    image: NetworkImage(photos[i],),
                                     fit: BoxFit.cover,
                                   ),
                                 ));
                               }
-                              return _buildCarousel(images);
+                              return Carousel(
+                                images: images,
+                                dotSize: 4.0,
+                                dotSpacing: 15.0,
+                                dotColor: Colors.lightGreenAccent,
+                                indicatorBgPadding: 5.0,
+                                dotBgColor: Colors.transparent,
+                                borderRadius: true,
+                              );
                             } else {
                               return Center(
                                 child: new CircularProgressIndicator(),
@@ -173,6 +166,7 @@ class HomePage extends StatelessWidget {
                           })
                     ]),
                   ),
+
                   viewModel.homeList.homelist != null
                       ? ListView.builder(
                           shrinkWrap: true,
@@ -182,7 +176,8 @@ class HomePage extends StatelessWidget {
                             BuildContext context,
                             int index,
                           ) {
-                            String langCode = Localizations.localeOf(context).languageCode;
+                            String langCode =
+                                Localizations.localeOf(context).languageCode;
                             if (langCode == "tr") {
                               title = viewModel.homeList.homelist[index].name_az
                                   .trim();
@@ -236,70 +231,10 @@ class HomePage extends StatelessWidget {
                             ));
                           })
                       : Center(child: CircularProgressIndicator()),
-                  // Container(child: Text("© 2019 Agro Trade",style: TextStyle(color: Colors.green),textAlign: TextAlign.end,),padding: EdgeInsets.all(8.0))
-
-                  //  _buildCard()
                 ]),
               ));
         });
     // TODO: implement build
-  }
-
-  static const platform = const MethodChannel("kendden_shehere/chat_activity");
-
-  _getNewActivity() async {
-    try {
-      await platform.invokeMethod('startNewActivity');
-    } on PlatformException catch (e) {
-      print(e.message);
-    }
-  }
-
-  Widget _buildCarousel(List<Widget> images) => Container(
-          child: new Stack(
-        children: <Widget>[
-          CarouselSlider(
-            pauseAutoPlayOnTouch: const Duration(seconds: 1),
-            items: images,
-            autoPlay: true,
-            height: 200,
-            aspectRatio: 2.0,
-            onPageChanged: (index) {
-//              setState(() {
-//                _current = index;
-//              });
-            },
-          ),
-          Positioned(
-              bottom: 0.0,
-              left: 0.0,
-              right: 0.0,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: map<Widget>(images, (index, url) {
-                  return Container(
-                    width: 8.0,
-                    height: 8.0,
-                    margin:
-                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
-                    decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: _current == index
-                            ? Colors.white
-                            : const Color(0xFFABC2B1)),
-                  );
-                }),
-              ))
-        ],
-      ));
-
-  List<T> map<T>(List list, Function handler) {
-    List<T> result = [];
-    for (var i = 0; i < list.length; i++) {
-      result.add(handler(i, list[i]));
-    }
-
-    return result;
   }
 
   _titleContainer(String title) => new Container(
@@ -317,63 +252,26 @@ class HomePage extends StatelessWidget {
               ),
               alignment: AlignmentDirectional.centerStart,
             ),
-//            new Container(
-//              child: new Text(
-//                "See All ",
-//                textAlign: TextAlign.left,
-//                style: new TextStyle(fontSize: 15),
-//              ),
-//              alignment: AlignmentDirectional.centerEnd,
-//            )
           ],
         ),
       );
 
-//  _buildCard() => new Container(
-//        padding: EdgeInsets.all(8.0),
-//        child: new ListView.builder(
-//          scrollDirection: Axis.horizontal,
-//          controller: _scrollController,
-//          itemBuilder: (BuildContext context, int index) {
-//            return Container(
-//                width: MediaQuery.of(context).size.width * 0.5,
-//                height: 360,
-//                child: InkWell(
-//                  child: GroceryListItemOne(
-//                    product: productListOne[index],
-//                  ),
-//                ));
-//          },
-//          itemCount: productListOne.length,
-//        ),
-//        width: width,
-//        height: 360,
-//      );
+  List<T> map<T>(List list, Function handler) {
+    List<T> result = [];
+    for (var i = 0; i < list.length; i++) {
+      result.add(handler(i, list[i]));
+    }
 
-//  void loadMore(String id) {
-//    page = page + 10;
-//    print(productListOne.toString() + "initial");
-//    viewModel.onFetchProductList(id, "0", "66", page.toString(), "0");
-//  }
-//
-//  _scrollListener() {
-//    if (_scrollController.offset >=
-//            _scrollController.position.maxScrollExtent &&
-//        !_scrollController.position.outOfRange) {
-//      setState(() {
-//        message = "reach the bottom";
-//        print(message);
-//        loadMore("66");
-//      });
-//    }
-//    if (_scrollController.offset <=
-//            _scrollController.position.minScrollExtent &&
-//        !_scrollController.position.outOfRange) {
-//      setState(() {
-//        message = "reach the top";
-//        print(message);
-//      });
-//    }
-//  }
+    return result;
+  }
+
+  static const platform = const MethodChannel("kendden_shehere/chat_activity");
+
+  _getNewActivity() async {
+    try {
+      await platform.invokeMethod('startNewActivity');
+    } on PlatformException catch (e) {
+      print(e.message);
+    }
+  }
 }
-//Search Widget
