8f2beafcebd29cbca35f6634da90ed87a7172a5c
konuleminova <konul.eminova39@gmail.com>
2019-10-17 15:29:10 +0400

    checkout done

--- lib/ui/page/payment/webview.dart
+++ lib/ui/page/payment/webview.dart
@@ -6,59 +6,74 @@ import 'package:kendden_shehere/ui/page/grocery/grocery_shop_list.dart';
 import 'package:kendden_shehere/ui/page/home.dart';
 //import 'package:webview_flutter/webview_flutter.dart';
 
-class WebViewPage extends StatefulWidget {
+//class WebViewPage extends StatefulWidget {
+//  String url;
+//
+//  WebViewPage({this.url});
+//
+//  @override
+//  State<StatefulWidget> createState() {
+//    // TODO: implement createState
+//    return WebViewState();
+//  }
+//}
+
+class WebViewPage extends StatelessWidget {
   String url;
 
   WebViewPage({this.url});
-
-  @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return WebViewState();
-  }
-}
-
-class WebViewState extends State<WebViewPage> {
-
-  @override
-  void initState() {
-    super.initState();
-    //flutterWebViewPlugin.close();
-    final flutterWebviewPlugin = new FlutterWebviewPlugin();
-    flutterWebviewPlugin.onUrlChanged.listen((String url) {
-      print(url);
-      if (widget.url != url) {
-        flutterWebviewPlugin.close();
-        flutterWebviewPlugin.dispose();
-        Route route = MaterialPageRoute(
-            builder: (BuildContext context) => HomePage(
-                  fromCheckout: true,
-                ));
-
-        Navigator.pushReplacement(context, route);
-      }
-    });
-  }
-
-  @override
-  void dispose() {
-    // flutterWebViewPlugin.dispose();
-
-    super.dispose();
-  }
+//  @override
+//  void initState() {
+//    super.initState();
+//    //flutterWebViewPlugin.close();
+//    final flutterWebviewPlugin = new FlutterWebviewPlugin();
+//    flutterWebviewPlugin.onUrlChanged.listen((String url) {
+//      print("printt"+url);
+//      if (widget.url != url) {
+//        flutterWebviewPlugin.close();
+//        flutterWebviewPlugin.dispose();
+//        Route route = MaterialPageRoute(
+//            builder: (BuildContext context) => HomePage(
+//                  fromCheckout: true,
+//                ));
+//
+//        Navigator.pushReplacement(context, route);
+//      }
+//    });
+//  }
+//
+//  @override
+//  void dispose() {
+//    // flutterWebViewPlugin.dispose();
+//
+//    super.dispose();
+//  }
 
   @override
   Widget build(BuildContext context) {
+    final flutterWebviewPlugin = new FlutterWebviewPlugin();
+    flutterWebviewPlugin.onUrlChanged.listen((String urls) {
+      print("printt"+urls);
+      if (urls != url) {
+        Route route = MaterialPageRoute(
+            builder: (BuildContext context) => HomePage(
+              fromCheckout: true,
+            ));
+
+        Navigator.pushReplacement(context, route);
+        flutterWebviewPlugin.close();
+        flutterWebviewPlugin.dispose();
+      }
+    });
     // TODO: implement build
     return new WebviewScaffold(
-        url: widget.url,
+        url:url,
         appBar: new AppBar(
           backgroundColor: Colors.lightGreen,
           title: const Text('Payment'),
         ),
         withZoom: true,
         withLocalStorage: true,
-        hidden: true,
         initialChild: Container(
           child: const Center(
             child: Text('Waiting.....'),
