ff82702a5ce77e8da938e9b71a1c7982d13ebab0
konuleminova <konul.eminova39@gmail.com>
2019-10-21 13:14:31 +0400

    debuging

--- lib/ui/widgets/list_item/glistitem3.dart
+++ lib/ui/widgets/list_item/glistitem3.dart
@@ -26,11 +26,12 @@ class NewGroceryListItemTwoState extends State<GroceryListItemThree> {
   String image, title;
   bool isAdded = false, isLiked = true;
   ProductViewModel viewModel;
-Store<AppState>store;
+  Store<AppState> store;
+
   @override
   Widget build(BuildContext context) {
     product = widget.product;
-    product.isAdded=true;
+    product.isAdded = true;
     image = "https://kenddenshehere.az/images/pr/th/" + product.code + ".jpg";
     String langCode = Localizations.localeOf(context).languageCode;
     if (langCode == "tr") {
@@ -43,10 +44,9 @@ Store<AppState>store;
 
     // TODO: implement build
     return StoreConnector(
-      onInit: (store){
-        this.store=store;
-
-      },
+        onInit: (store) {
+          this.store = store;
+        },
         onInitialBuild: (ProductViewModel viewModel) {
           this.viewModel = viewModel;
         },
@@ -56,204 +56,214 @@ Store<AppState>store;
         converter: (Store<AppState> store) => ProductViewModel.create(store),
         builder: (BuildContext context, ProductViewModel viewModel) {
           return product != null
-              ? GestureDetector(
-                  child: Card(
-                    margin: EdgeInsets.all(12),
-                    child: Material(
-                        // borderRadius: BorderRadius.circular(20.0),
-                        elevation: 4.0,
-                        child: new Container(
-                            height: 120,
-                            color: Colors.white,
-                            //  decoration: BoxDecoration( borderRadius: BorderRadius.circular(20.0),),
-                            child: Row(
-                              children: <Widget>[
-                                Expanded(
-                                  child: ListTile(
-                                    leading: Container(
-                                      child: FadeInImage.assetNetwork(
-                                        image: image,
-                                        placeholder: "images/noimage.png",
-                                        fit: BoxFit.cover,
-                                        width:
-                                        MediaQuery.of(context).size.width *
-                                            0.2,
-                                      ),
-                                    ),
-                                    title: Container(
-                                      height: 110.0,
-                                      child: Column(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.center,
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: <Widget>[
-                                          new GroceryTitle(text: title),
-                                          new GrocerySubtitle(
-                                              text: product.price + " AZN"),
-                                          new GrocerySubtitle(
-                                              text: product.counttype),
-                                        ],
-                                      ),
+              ? Card(
+                  margin: EdgeInsets.all(12),
+                  child: Material(
+                      // borderRadius: BorderRadius.circular(20.0),
+                      elevation: 4.0,
+                      child: new Container(
+                          height: 120,
+                          color: Colors.white,
+                          //  decoration: BoxDecoration( borderRadius: BorderRadius.circular(20.0),),
+                          child: Row(
+                            children: <Widget>[
+                              Expanded(
+                                child: ListTile(
+                                  leading: Container(
+                                    child: FadeInImage.assetNetwork(
+                                      image: image,
+                                      placeholder: "images/noimage.png",
+                                      fit: BoxFit.cover,
+                                      width: MediaQuery.of(context).size.width *
+                                          0.2,
                                     ),
                                   ),
-                                  flex: 3,
+                                  title: Container(
+                                    height: 110.0,
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: <Widget>[
+                                        new GroceryTitle(text: title),
+                                        new GrocerySubtitle(
+                                            text: product.price + " AZN"),
+                                        new GrocerySubtitle(
+                                            text: product.counttype),
+                                      ],
+                                    ),
+                                  ),
+                                  onTap: (){
+                                    Route route = MaterialPageRoute(
+                                        builder: (BuildContext context) =>
+                                            GroceryDetailsPage(product));
+                                    Navigator.push(context, route);
+                                  },
                                 ),
-                                Expanded(
-                                    child: Container(
-                                  margin: EdgeInsets.only(right: 16),
-                                  child: Column(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceAround,
-                                    crossAxisAlignment: CrossAxisAlignment.end,
-                                    children: <Widget>[
-                                      IconButton(
-                                        icon: Icon(
-                                          Icons.delete_outline,
-                                          color: Colors.green[400],
-                                          size: 25,
-                                        ),
-                                        onPressed: () {
-                                          Networks()
-                                              .removeFromBasket(product.id)
-                                              .then((onvalue) {
-                                           if(onvalue!=null){
-                                             if (onvalue['action'] ==
-                                                 "done") {
-                                               viewModel.removeShopItem(product);
-                                             }
-                                           }
-                                          });
-
-                                        },
+                                flex: 5,
+                              ),
+                              Expanded(
+                                flex: 2,
+                                  child: Container(
+                                margin: EdgeInsets.only(right: 16),
+                                child: Column(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceAround,
+                                  crossAxisAlignment: CrossAxisAlignment.end,
+                                  children: <Widget>[
+                                    IconButton(
+                                      icon: Icon(
+                                        Icons.delete_outline,
+                                        color: Colors.green[400],
+                                        size: 25,
                                       ),
-                                      _updateContainer()
-                                    ],
-                                  ),
-                                  height: MediaQuery.of(context).size.height,
-                                ))
-                              ],
-                            ))),
-                  ),
-                  onTap: () {
-                    Route route = MaterialPageRoute(
-                        builder: (BuildContext context) =>
-                            GroceryDetailsPage(product));
-                    Navigator.push(context, route);
-                  },
+                                      onPressed: () {
+                                        Networks()
+                                            .removeFromBasket(product.id)
+                                            .then((onvalue) {
+                                          if (onvalue != null) {
+                                            if (onvalue['action'] == "done") {
+                                              viewModel.removeShopItem(product);
+                                            }
+                                          }
+                                        });
+                                      },
+                                    ),
+                                    _updateContainer()
+                                  ],
+                                ),
+                                height: MediaQuery.of(context).size.height,
+                              ))
+                            ],
+                          ))),
                 )
               : SizedBox();
         });
   }
 
   _updateContainer() {
-    int weight=product.weight;
+    int weight = product.weight;
     if (!product.isAdded) {
       return new GestureDetector(
-        child: new Container(
           child: new Container(
-            padding: EdgeInsets.all(8),
-            color: Colors.lightGreen,
-            child: new SizedBox(
-              child: new Icon(
-                Icons.shopping_cart,
-                color: Colors.white,
+            child: new Container(
+              padding: EdgeInsets.all(8),
+              color: Colors.lightGreen,
+              child: new SizedBox(
+                child: new Icon(
+                  Icons.shopping_cart,
+                  color: Colors.white,
+                ),
+                height: 20,
+                width: 20,
               ),
-              height: 20,
-              width: 20,
             ),
           ),
-        ),
-        onTap: ()  {
-          Networks()
-              .addToBasket(product.id, product.weight.toString())
-              .then((onvalue) {
-            print(onvalue);
-            if (onvalue != null) {
-              if (onvalue['action'] == "done") {
-                viewModel.addShopItem(product);
-                // viewModel.onFetchShopList();
-                setState(() {
-                  product.isAdded = !product.isAdded;
-                });
-                //viewModel.changeAddStatus(index, true, product.weight);
+          onTap: () {
+            Networks()
+                .addToBasket(product.id, product.weight.toString())
+                .then((onvalue) {
+              print(onvalue);
+              if (onvalue != null) {
+                if (onvalue['action'] == "done") {
+                  viewModel.addShopItem(product);
+                  // viewModel.onFetchShopList();
+                  setState(() {
+                    product.isAdded = !product.isAdded;
+                  });
+                  //viewModel.changeAddStatus(index, true, product.weight);
+                }
               }
-            }
+            });
           });
-        }
-      );
     } else {
       return new Container(
-        width: 80,
-        padding: EdgeInsets.all(2),
-        margin: EdgeInsets.only(top: 8, bottom: 8),
+        // padding: EdgeInsets.all(4),
+        // margin: EdgeInsets.only(top: 8, bottom: 8),
         decoration: new BoxDecoration(
             borderRadius: BorderRadius.circular(2),
             color: Colors.white,
             border: Border.all(color: Colors.grey)),
         alignment: Alignment.topRight,
         child: new Row(
+          mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            new GestureDetector(
-              child: new Icon(Icons.remove),
-              onTap: () {
-                weight--;
-                if (weight < 1) {
-                  weight = 1;
-                  Networks().removeFromBasket(product.id).then((onvalue) {
-                    print("REMOVE");
-                    if (onvalue != null) {
-                      if (onvalue['action'] == "done") {
-                        viewModel.removeShopItem(product);
-                      }
-                    }
-                  });
-                } else {
-                  Networks()
-                      .addToBasket(product.id, weight.toString())
-                      .then((onvalue) {
-                    if (onvalue != null) {
-                      if (onvalue['action'] == "done") {
-                        store.dispatch(ShowBasketAction(store));
-                        setState(() {
-                          // product.isAdded = !product.isAdded;
-                          product.weight--;
-                        });
-                        //viewModel.changeAddStatus(index, true, weight);
-                      }
-                    }
-                  });
-                }
-              },
-            ),
-            new Text(
-              product.weight.toString(),
-              style: new TextStyle(fontSize: 18),
-            ),
-            new GestureDetector(
-              child: new Icon(Icons.add),
-              onTap: () {
-                weight++;
-                Networks()
-                    .addToBasket(product.id, weight.toString())
-                    .then((onvalue) {
-                  if (onvalue != null) {
-                    if (onvalue['action'] == "done") {
-                      //  viewModel.changeAddStatus(index, true, weight);
-                      //viewModel.onFetchShopList();
-                      store.dispatch(ShowBasketAction(store));
-                      setState(() {
-                        product.weight++;
+            Expanded(
+                child: Container(
+                    child: IconButton(
+                      icon: new Icon(Icons.remove),
+                      iconSize: 20,
+                      onPressed: () {
+                        weight--;
+                        if (weight < 1) {
+                          weight = 1;
+                          Networks()
+                              .removeFromBasket(product.id)
+                              .then((onvalue) {
+                            print("REMOVE");
+                            if (onvalue != null) {
+                              if (onvalue['action'] == "done") {
+                                viewModel.removeShopItem(product);
+                              }
+                            }
+                          });
+                        } else {
+                          Networks()
+                              .addToBasket(product.id, weight.toString())
+                              .then((onvalue) {
+                            if (onvalue != null) {
+                              if (onvalue['action'] == "done") {
+                                store.dispatch(ShowBasketAction(store));
+                                setState(() {
+                                  // product.isAdded = !product.isAdded;
+                                  product.weight--;
+                                });
+                                //viewModel.changeAddStatus(index, true, weight);
+                              }
+                            }
+                          });
+                        }
+                      },
+                    ),)),
+            SizedBox(width: 10,),
+            Expanded(
+                child: Container(
+                  child: new Text(
+                    product.weight.toString(),
+                    textAlign: TextAlign.center,
+                    style: new TextStyle(
+                      fontSize: 16,
+                    ),
+                  ),
+                )),
+            Expanded(
+                child: Container(
+                  child: IconButton(
+                    icon: new Icon(Icons.add),
+                    iconSize: 20,
+                    onPressed: () {
+                      weight++;
+                      Networks()
+                          .addToBasket(product.id, weight.toString())
+                          .then((onvalue) {
+                        if (onvalue != null) {
+                          if (onvalue['action'] == "done") {
+                            //  viewModel.changeAddStatus(index, true, weight);
+                            //viewModel.onFetchShopList();
+                            store.dispatch(ShowBasketAction(store));
+                            setState(() {
+                              product.weight++;
+                            });
+                          }
+                        }
                       });
-
-                    }
-                  }
-                });
-                //Networks().addToBasket(product.id, amount.toString());
-              },
-            ),
+                      //Networks().addToBasket(product.id, amount.toString());
+                    },
+                  ),
+                )),
+            SizedBox(width: 8,)
           ],
         ),
       );
