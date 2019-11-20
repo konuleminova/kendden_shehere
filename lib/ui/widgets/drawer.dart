a5f568ab4edfa54e34fe20a3e107b82cb095ac8b
konuleminova <konul.eminova39@gmail.com>
2019-11-20 15:08:12 +0400

    menu

--- lib/ui/widgets/drawer.dart
+++ lib/ui/widgets/drawer.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:kendden_shehere/constants/Constants.dart';
 import 'package:kendden_shehere/localization/app_translations.dart';
 import 'package:kendden_shehere/service/networks.dart';
 import 'package:kendden_shehere/ui/page/grocery/grocery_categories.dart';
+import 'package:kendden_shehere/ui/page/menu/profile.dart';
 import 'package:kendden_shehere/util/sharedpref_util.dart';
 
 class DrawerWidget extends StatelessWidget {
@@ -12,76 +14,12 @@ class DrawerWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return new Drawer(
-      child: ListView(
+        child: Container(
+      color: greenFixed,
+      child: Column(
         children: <Widget>[
           new Stack(
             children: <Widget>[
-              new FutureBuilder(
-                future: _getData(),
-                builder: (BuildContext context, AsyncSnapshot snapshot) {
-                  if (snapshot.hasData) {
-                    final UriData data =
-                        Uri.parse(snapshot.data[1]['img']).data;
-                    return UserAccountsDrawerHeader(
-                      accountName: snapshot.hasData
-                          ? new Text(
-                              snapshot.data[1]['name'] +
-                                  " " +
-                                  snapshot.data[1]['surname'],
-                              style: new TextStyle(fontSize: 18),
-                            )
-                          : new Text(
-                              name + " " + surname,
-                              style: new TextStyle(fontSize: 18),
-                            ),
-                      accountEmail: new Text(""),
-                      currentAccountPicture: CircleAvatar(
-                          minRadius: 60,
-                          backgroundColor: Colors.green.shade300,
-                          child: data != null
-                              ? ClipOval(
-                                  child: new Image.memory(data.contentAsBytes(),
-                                      width: 60,
-                                      height: 60,
-                                      fit: BoxFit.fill),
-                                )
-                              : CircleAvatar(
-                                  radius: 50.0,
-                                  backgroundImage:
-                                      AssetImage('images/profile.png'),
-                                  backgroundColor: Colors.transparent,
-                                )),
-                      decoration: new BoxDecoration(color: Colors.lightGreen),
-                      onDetailsPressed: () {
-                        Navigator.pop(context);
-                        Navigator.pushNamed(context, "/profile");
-                      },
-                    );
-                  } else {
-                    // default show loading while state is waiting
-                    return UserAccountsDrawerHeader(
-                      accountName: new Text(
-                        name + " " + surname,
-                        style: new TextStyle(fontSize: 18),
-                      ),
-                      accountEmail: new Text(""),
-                      currentAccountPicture: CircleAvatar(
-                          minRadius: 60,
-                          backgroundColor: Colors.green.shade300,
-                          child: CircleAvatar(
-                            radius: 50.0,
-                            backgroundImage: AssetImage('images/profile.png'),
-                            backgroundColor: Colors.transparent,
-                          )),
-                      decoration: new BoxDecoration(color: Colors.lightGreen),
-                      onDetailsPressed: () {
-                        Navigator.pop(context);
-                        Navigator.pushNamed(context, "/profile");
-                      },
-                    );
-                  }
-                },
-              ),
               Container(
                 child: new IconButton(
                   icon: new Icon(Icons.settings),
@@ -96,90 +34,158 @@ class DrawerWidget extends StatelessWidget {
               )
             ],
           ),
+          DrawerHeader(child: Container()),
           ListTile(
-            leading: Icon(Icons.category),
-            title: Text(AppTranslations.of(context).text("categories")),
+            contentPadding: EdgeInsets.only(left: 30.0),
+            title: Text(
+              "Profile",
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
             onTap: () {
               Navigator.pop(context);
               Navigator.push(
                   context,
                   MaterialPageRoute(
-                      builder: (BuildContext context) =>
-                          new GroceryCategoriesPage(
-                              id: "0",
-                              title: AppTranslations.of(context)
-                                  .text("categories"))));
+                      builder: (BuildContext context) => ProfilePage()));
               // Navigator.pushNamed(context, "/categories");
             },
           ),
-          GestureDetector(
-            child: ListTile(
-              leading: Icon(Icons.favorite),
-              title: Text(AppTranslations.of(context).text("wish_list")),
+          Padding(
+            padding: EdgeInsets.only(left: 30.0, right: 30),
+            child: Divider(
+              color: Colors.white,
             ),
+          ),
+          ListTile(
+            contentPadding: EdgeInsets.only(left: 30.0),
             onTap: () {
               Navigator.pop(context);
-              Navigator.pushNamed(context, "/wish_list");
+              Navigator.pushNamed(
+                context,
+                "/wish_list",
+              );
             },
-          ),
-          GestureDetector(
-            child: ListTile(
-              leading: Icon(Icons.history),
-              title: Text(AppTranslations.of(context).text("order_history")),
+            title: Text(
+              AppTranslations.of(context).text(
+                "wish_list",
+              ),
+              style: TextStyle(color: Colors.white, fontSize: 20),
             ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 30.0, right: 30.0),
+            child: Divider(
+              color: Colors.white,
+            ),
+          ),
+          ListTile(
+            contentPadding: EdgeInsets.only(left: 30.0),
             onTap: () {
               Navigator.pop(context);
               Navigator.pushNamed(context, "/order_history");
             },
+            title: Text(
+              "My Cart",
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 30.0, right: 30.0),
+            child: Divider(
+              color: Colors.white,
+            ),
           ),
           ListTile(
-            leading: Icon(Icons.feedback),
-            title: Text(AppTranslations.of(context).text("delivery_terms")),
+            contentPadding: EdgeInsets.only(left: 30.0),
+            title: Text(
+              AppTranslations.of(context).text("delivery_terms"),
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
             onTap: () {
               Navigator.pop(context);
               Navigator.pushNamed(context, "/delivery_terms");
             },
           ),
+          Padding(
+            padding: EdgeInsets.only(left: 30.0, right: 30.0),
+            child: Divider(
+              color: Colors.white,
+            ),
+          ),
           ListTile(
-            leading: Icon(Icons.question_answer),
-            title: Text(AppTranslations.of(context).text("fag")),
+            contentPadding: EdgeInsets.only(left: 30.0),
+            title: Text(AppTranslations.of(context).text("fag"),
+                style: TextStyle(color: Colors.white, fontSize: 20)),
             onTap: () {
               Navigator.pop(context);
               Navigator.pushNamed(context, "/fag");
             },
           ),
+          Padding(
+            padding: EdgeInsets.only(left: 30.0, right: 30.0),
+            child: Divider(
+              color: Colors.white,
+            ),
+          ),
           ListTile(
-            leading: Icon(Icons.info),
-            title: Text(AppTranslations.of(context).text("about_us")),
+            contentPadding: EdgeInsets.only(left: 30.0),
+            title: Text(
+              AppTranslations.of(context).text("about_us"),
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
             onTap: () {
               Navigator.pop(context);
               Navigator.pushNamed(context, "/about_us");
             },
           ),
+          Padding(
+            padding: EdgeInsets.only(left: 30.0, right: 30.0),
+            child: Divider(
+              color: Colors.white,
+            ),
+          ),
           ListTile(
-            leading: Icon(Icons.phone),
-            title: Text(AppTranslations.of(context).text("contact_us")),
+            contentPadding: EdgeInsets.only(left: 30.0),
+            title: Text(
+              AppTranslations.of(context).text("contact_us"),
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
             onTap: () {
               Navigator.pop(context);
               Navigator.pushNamed(context, "/contacts");
             },
           ),
+          Padding(
+            padding: EdgeInsets.only(left: 30.0, right: 30.0),
+            child: Divider(
+              color: Colors.white,
+            ),
+          ),
           ListTile(
-            leading: Icon(Icons.people),
-            title: Text(AppTranslations.of(context).text("complaints")),
+            contentPadding: EdgeInsets.only(left: 30.0),
+            title: Text(
+              AppTranslations.of(context).text("complaints"),
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
             onTap: () {
               Navigator.pop(context);
               Navigator.pushNamed(context, "/complaints");
             },
           ),
+          Padding(
+            padding: EdgeInsets.only(left: 30.0, right: 30.0),
+            child: Divider(
+              color: Colors.white,
+            ),
+          ),
         ],
       ),
-    );
+    ));
   }
-
-  _getData() {
-    name = SharedPrefUtil().name;
-    surname = SharedPrefUtil().surname;
-    return Networks().userinfo();
-  }
+//
+//  _getData() {
+//    name = SharedPrefUtil().name;
+//    surname = SharedPrefUtil().surname;
+//    return Networks().userinfo();
+//  }
 }
