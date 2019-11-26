fd7f35b96525106d2afa07c922ea94b2d13099f9
konuleminova <konul.eminova39@gmail.com>
2019-11-26 17:48:27 +0400

    edit profile

--- lib/ui/page/menu/settings.dart
+++ lib/ui/page/menu/settings.dart
@@ -31,7 +31,7 @@ class SettingsState extends State<SettingsPage> {
               Positioned(
                 child: new Container(
                     child: new Text(
-                     AppTranslations.of(context).text('select_lang'),
+                      AppTranslations.of(context).text('select_lang'),
                       style: TextStyle(fontSize: 20, color: Colors.green),
                     ),
                     margin: EdgeInsets.only(left: 16, right: 16, top: 16)),
@@ -47,25 +47,32 @@ class SettingsState extends State<SettingsPage> {
                 top: 45,
               ),
               new Positioned(
-                  bottom: 0,
-                  right: 0,
-                  left: 0,
-                  child: Container(
-                    margin: EdgeInsets.all(16),
-                    width: MediaQuery.of(context).size.width,
-                    child: RaisedButton(
-                      color: Colors.green,
+                bottom: 0,
+                right: 0,
+                left: 0,
+                child: Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.all(30.0),
+                  child: RaisedButton(
+                      padding: EdgeInsets.symmetric(vertical: 16.0),
+                      color: greenFixed,
+                      disabledColor: greenFixed,
                       onPressed: () {
-                        SharedPrefUtil().setString(SharedPrefUtil().lang, _value);
+                        SharedPrefUtil()
+                            .setString(SharedPrefUtil().lang, _value);
                         application.onLocaleChanged(Locale(_value, ""));
                         Navigator.pop(context);
                       },
+                      elevation: 8,
+                      shape: RoundedRectangleBorder(
+                          borderRadius:
+                              BorderRadius.all(Radius.circular(40.0))),
                       child: Text(
                         AppTranslations.of(context).text('save'),
                         style: TextStyle(color: Colors.white),
-                      ),
-                    ),
-                  ))
+                      )),
+                ),
+              )
             ],
           ),
         ));
@@ -119,7 +126,7 @@ class SettingsState extends State<SettingsPage> {
   @override
   void initState() {
     super.initState();
-   SharedPrefUtil().getString(SharedPrefUtil().lang).then((onvalue) {
+    SharedPrefUtil().getString(SharedPrefUtil().lang).then((onvalue) {
       setState(() {
         if (onvalue == "") {
           _value = "en";
