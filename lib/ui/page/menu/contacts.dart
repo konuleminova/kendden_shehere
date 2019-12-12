4e6a301ecb39c7af63835ceddaaad91a0c5f3e02
konuleminova <konul.eminova39@gmail.com>
2019-12-12 11:17:37 +0400

    contact us

--- lib/ui/page/menu/contacts.dart
+++ lib/ui/page/menu/contacts.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_launch/flutter_launch.dart';
 import 'package:flutter_open_whatsapp/flutter_open_whatsapp.dart';
 import 'package:kendden_shehere/constants/Constants.dart';
 import 'package:kendden_shehere/localization/app_translations.dart';
@@ -19,19 +20,18 @@ class ContactsPage extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
               ListTile(
-                leading: Image.asset(
-                  'images/ks/wp.png',
-                ),
-                title: Text(AppTranslations.of(context).text('wp')),
-                trailing: Icon(
-                  Icons.arrow_forward_ios,
-                  size: 18,
-                  color: greenFixed,
-                ),
-                onTap: (){
-                  whatsAppOpen();
-                }
-              ),
+                  leading: Image.asset(
+                    'images/ks/wp.png',
+                  ),
+                  title: Text(AppTranslations.of(context).text('wp')),
+                  trailing: Icon(
+                    Icons.arrow_forward_ios,
+                    size: 18,
+                    color: greenFixed,
+                  ),
+                  onTap: () {
+                    whatsAppOpen();
+                  }),
               Padding(
                 padding: EdgeInsets.only(left: 60),
                 child: Divider(
@@ -48,6 +48,10 @@ class ContactsPage extends StatelessWidget {
                   size: 18,
                   color: greenFixed,
                 ),
+                onTap: ()  {
+//                  await FlutterLaunch.launchWathsApp(
+//                      phone: "5534992019999", message: "Hello");
+                },
               ),
               Padding(
                 padding: EdgeInsets.only(left: 60),
@@ -113,7 +117,8 @@ class ContactsPage extends StatelessWidget {
           ),
         ));
   }
-  void whatsAppOpen(){
+
+  void whatsAppOpen() {
     FlutterOpenWhatsapp.sendSingleMessage("918179015345", "Hello");
   }
 }
