2b1c4716359a06d5e10c73f26c1838c24d7f3545
konuleminova <konul.eminova39@gmail.com>
2019-12-12 10:40:02 +0400

    contact us

--- lib/ui/page/menu/contacts.dart
+++ lib/ui/page/menu/contacts.dart
@@ -7,56 +7,110 @@ import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:kendden_shehere/util/helper_class.dart';
 
 class ContactsPage extends StatelessWidget {
-  String lang;
-
   @override
   Widget build(BuildContext context) {
-    String langCode = Localizations.localeOf(context).languageCode;
-    if (langCode == "tr") {
-      lang = "0";
-    } else if (langCode == "en") {
-      lang = "2";
-    } else if (langCode == "ru") {
-      lang = "1";
-    }
-
     // TODO: implement build
     return Scaffold(
-      backgroundColor: greyFixed,
-      appBar: AppBar(
-        title: Text(AppTranslations.of(context).text("contact_us")),
-        backgroundColor: greenFixed,
-      ),
-      body: Padding(
-          padding: EdgeInsets.all(8.0),
-          child: Card(
-              elevation: 10,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(4)),
-              child: new FutureBuilder(
-                  future: Networks().contacts(lang),
-                  builder: (BuildContext context, AsyncSnapshot snapshot) {
-                    if (snapshot.hasData) {
-                      // ListInfo information = snapshot.data;
-                      if (snapshot.data != null) {
-                        String header = snapshot.data['header'] ?? "";
-                        String body = snapshot.data['body'] ?? "";
-                        String markdown = html2md.convert(body);
-                        return SingleChildScrollView(
-                          child: new Container(
-                              margin: EdgeInsets.all(16),
-                              child: new MarkdownBody(
-                                data: markdown,
-                              )),
-                        );
-                      }
-                    } else if (snapshot.connectionState ==
-                        ConnectionState.waiting) {
-                      return loading();
-                    } else {
-                      return Container();
-                    }
-                  }))),
-    );
+        backgroundColor: greyFixed,
+        appBar: AppBar(
+          title: Text(AppTranslations.of(context).text("contact_us")),
+          backgroundColor: greenFixed,
+        ),
+        body: Container(
+          margin: EdgeInsets.only(left: 16, top: 24, bottom: 16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              ListTile(
+                leading: Image.asset(
+                  'images/ks/wp.png',
+                ),
+                title: Text(AppTranslations.of(context).text('wp')),
+                trailing: Icon(
+                  Icons.arrow_forward_ios,
+                  size: 18,
+                  color: greenFixed,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 60),
+                child: Divider(
+                  color: Colors.grey,
+                ),
+              ),
+              ListTile(
+                leading: Image.asset(
+                  'images/ks/call.png',
+                ),
+                title: Text(AppTranslations.of(context).text('call')),
+                trailing: Icon(
+                  Icons.arrow_forward_ios,
+                  size: 18,
+                  color: greenFixed,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 60),
+                child: Divider(
+                  color: Colors.grey,
+                ),
+              ),
+              ListTile(
+                leading: Image.asset(
+                  'images/ks/insta.png',
+                ),
+                title: Text(AppTranslations.of(context).text('insta')),
+                trailing: Icon(
+                  Icons.arrow_forward_ios,
+                  size: 18,
+                  color: greenFixed,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 60),
+                child: Divider(
+                  color: Colors.grey,
+                ),
+              ),
+              ListTile(
+                leading: Image.asset(
+                  'images/ks/fb.png',
+                ),
+                title: Text(AppTranslations.of(context).text('fb')),
+                trailing: Icon(
+                  Icons.arrow_forward_ios,
+                  size: 18,
+                  color: greenFixed,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 60),
+                child: Divider(
+                  color: Colors.grey,
+                ),
+              ),
+              ListTile(
+                leading: Image.asset(
+                  'images/ks/email.png',
+                ),
+                title: Text(AppTranslations.of(context).text('email')),
+                trailing: Icon(
+                  Icons.arrow_forward_ios,
+                  size: 18,
+                  color: greenFixed,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 60),
+                child: Divider(
+                  color: Colors.grey,
+                ),
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height * 0.3,
+              ),
+            ],
+          ),
+        ));
   }
 }
